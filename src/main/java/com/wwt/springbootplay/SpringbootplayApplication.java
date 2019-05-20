c656563b79d5ead58e9c4d4b760ff667f96f4c26
wentao.wang <wentao.wang@ly.com>
2019-05-20 11:10:37 +0800

    project can start

---
+++ src/main/java/com/wwt/springbootplay/SpringbootplayApplication.java
@@ -0,0 +1,13 @@
+package com.wwt.springbootplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootplayApplication.class, args);
+	}
+
+}
