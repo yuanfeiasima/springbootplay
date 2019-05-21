b5eae7af3cd950754e785fffd12f2bcac62a8e60
wentao.wang <wentao.wang@ly.com>
2019-05-21 14:30:34 +0800

    leetcode206

---
+++ src/main/java/com/wwt/springbootplay/algorithms/L206ReverseLinkedList.java
@@ -0,0 +1,52 @@
+package com.wwt.springbootplay.algorithms;
+
+import com.wwt.springbootplay.algorithms.base.ListNode;
+
+/**
+ * @author grace
+ * @date 2019-05-20 14:09
+ */
+public class L206ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        return reverse(head, null);
+    }
+
+    /**
+     * 递归
+     *
+     * @param current
+     * @param pre
+     * @return
+     */
+    private ListNode reverse(ListNode current, ListNode pre) {
+        if (current == null) {
+            return pre;
+        }
+        ListNode next = current.next;
+        current.next = pre;
+        return reverse(next, current);
+    }
+
+    /**
+     * 非递归
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // a -> b -> c -> d -> null
+        // a <- b <- c <- d
+        ListNode pre = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
---
+++ src/main/java/com/wwt/springbootplay/algorithms/base/ListNode.java
@@ -0,0 +1,14 @@
+package com.wwt.springbootplay.algorithms.base;
+
+/**
+ * @author grace
+ * @date 2019-05-20 14:13
+ */
+public class ListNode {
+    int val;
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
