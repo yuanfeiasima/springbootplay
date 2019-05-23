e50d0379ef7fa2ddad8607da37cdd77ee02be0a4
wentao.wang <wentao.wang@ly.com>
2019-05-23 11:11:33 +0800

    爬楼梯 动态规划

---
+++ src/main/java/com/wwt/springbootplay/algorithms/L70ClimbStair.java
@@ -0,0 +1,18 @@
+package com.wwt.springbootplay.algorithms;
+
+/**
+ * @author grace
+ * @date 2019-05-23 11:11
+ */
+public class L70ClimbStair {
+    public int climbStairs(int n) {
+        if(n == 1) return 1;
+        int[] dp= new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+}
