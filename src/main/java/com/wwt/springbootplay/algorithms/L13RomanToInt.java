22462ff117af9aa7d5af9f319dc136c07f2b29ed
wangwentao09 <wangwentao09@meituan.com>
2021-06-09 22:58:52 +0800

    罗马字母转数字

--- src/main/java/com/wwt/springbootplay/algorithms/L13RomanToInt.java
+++ src/main/java/com/wwt/springbootplay/algorithms/L13RomanToInt.java
@@ -39,4 +39,27 @@ public class L13RomanToInt {
         }
         return result;
     }
+
+    public int romanToIntV2(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int ans = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            int value = map.get(s.charAt(i));
+            if (i < n - 1 && value < map.get(s.charAt(i + 1))) {
+                ans -= value;
+            } else {
+                ans += value;
+            }
+        }
+        return ans;
+    }
 }
